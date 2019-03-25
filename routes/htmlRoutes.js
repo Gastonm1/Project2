db411f4952802663d68d75eaddce1d011c3698e3
B312 <11902197+davidscott321@users.noreply.github.com>
2019-03-25 05:32:59 -0500

    testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,19 @@
 var db = require("../models");
+// Testing
+var express = require('express'); 
+var router = express.Router(); 
 
 module.exports = function(app) {
+  // app.get('/test', function (req, res, next) {
+  //   res.render('signin', { title: 'MyApp', css:'style.css'});
+  // });
+
+  app.get("/test", function(req, res) {
+    res.render("example", {
+      title: 'MyApp', css:'style.css'
+    });
+  });
+
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
@@ -18,6 +31,25 @@ module.exports = function(app) {
         example: dbExample
       });
     });
+  });
+
+  app.get("/signin", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  app.get("/signin", function (req, res) {
+    res.locals.metaTags = { 
+      title: "Sign In || Sign Up",
+      description: "Handlebars description demo for your page",   
+      keywords: "Keywords for this page" ,
+      css: "style.css"
+    }; 
+    console.log(res.locals.metaTags.css);
+    res.render("signin", { layout: "main" }); 
   });
 
   // Render 404 page for any unmatched routes
